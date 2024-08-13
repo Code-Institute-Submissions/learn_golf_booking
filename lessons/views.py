d062fe4a57f9f8a53015519f33c9b0188f2d5a74
Lewis Clements <lewisclements15@icloud.com>
2024-08-13 19:28:42 +0000

    add update booking view

--- lessons/views.py
+++ lessons/views.py
@@ -13,3 +13,14 @@ def booking_create(request):
     else:
         form = BookingForm()
     return render(request, '', {'form': form})
+
+def booking_update(request, booking_id):
+    booking = get_object_or_404(Booking, pk=booking_id)
+    if request.method == "POST":
+        form = BookingForm(request.POST, instance=booking)
+        if form.is_valid():
+            booking = form.save()
+            return redirect('booking_detail', booking_id=booking.id)
+    else:
+        form = BookingForm(instance=booking)
+    return render(request, '', {'form': form})

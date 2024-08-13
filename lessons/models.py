76ac8ca28031e1f475a7bde2834eca9fcab672d8
Lewis Clements <lewisclements15@icloud.com>
2024-08-13 19:09:50 +0000

    Create booking model

--- lessons/models.py
+++ lessons/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Booking(Base):
+    __tablename__ = 'bookings'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    customer_name = Column(String(100), nullable=False)
+    booking_date = Column(Date, nullable=False)
+    booking_time = Column(Time, nullable=False)
+    created_at = Column(TIMESTAMP, server_default=func.now())
+
+    def __repr__(self):
+        return f"<Booking(id={self.id}, customer_name={self.customer_name}, date={self.booking_date}, time={self.booking_time})>"

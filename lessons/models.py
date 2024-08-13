c4b7f1fe49ed0bb376a6e34b32ce30f2e7808467
Lewis Clements <lewisclements15@icloud.com>
2024-08-13 19:38:20 +0000

    add url patterns

--- lessons/models.py
+++ lessons/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class Booking(Base):
+class Booking(models.Model):
     __tablename__ = 'bookings'
 
     id = Column(Integer, primary_key=True, autoincrement=True)

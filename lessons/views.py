1ea691fbd2364ef0940fb0d76bde51f5bf01e295
Lewis Clements <lewisclements15@icloud.com>
2024-08-13 19:26:41 +0000

    add create booking view

--- lessons/views.py
+++ lessons/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from models import Booking
+
 # Create your views here.
 
-def book_lessons(request):
-    return HttpResponse("Book a lesson!")
+def booking_create(request):
+    if request.method == "POST":
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            booking = form.save()
+            return redirect('booking_detail', booking_id=booking.id)
+    else:
+        form = BookingForm()
+    return render(request, '', {'form': form})

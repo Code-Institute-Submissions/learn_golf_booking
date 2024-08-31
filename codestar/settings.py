7fa289a4f2a764bcf77e8b5a50ee7e1fce377ffd
Lewis Clements <lewisclements15@icloud.com>
2024-08-31 12:03:42 +0000

    install whitenoise

--- codestar/settings.py
+++ codestar/settings.py
@@ -60,6 +60,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'lessons.middleware.DeletePastBookingsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'codestar.urls'
